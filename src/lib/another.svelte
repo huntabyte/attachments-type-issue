cec6ae8d683d219da8dc4a574d253bb16975f4f2
Hunter Johnston <johnstonhuntera@gmail.com>
2025-05-14 21:25:02 -0400

    repro

---
+++ src/lib/another.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { ComponentProps } from 'svelte';
+	import Component from './component.svelte';
+
+	let { extraProps }: { extraProps: { component?: Partial<ComponentProps<typeof Component>> } } =
+		$props();
+</script>
+
+<Component {...extraProps?.component} />
---
+++ src/lib/component.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	let {
+		children,
+		ref = $bindable(),
+		...restProps
+	}: HTMLAttributes<HTMLElement> & {
+		ref?: HTMLElement;
+	} = $props();
+</script>
+
+<div bind:this={ref} class="component" {...restProps}>
+	{@render children?.()}
+</div>
